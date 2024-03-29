d7649438dd69691c441ac795c5038102016137a6
Élisson Souza <elisson-souza@outlook.com>
2024-03-29 03:22:03 -0300

    Test new branch

--- src/App.java
+++ src/App.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
-
         int menu = 0;
         boolean concluido;
 
