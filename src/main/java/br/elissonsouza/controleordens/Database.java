e988017cb50b8274215f32c372604384d9272788
Élisson Souza <elisson-souza@outlook.com>
2024-04-09 18:05:41 -0300

    Conserta caminho para consulta dos arquivos referentes ao banco de dados

--- src/main/java/br/elissonsouza/controleordens/Database.java
+++ src/main/java/br/elissonsouza/controleordens/Database.java
@@ -1,5 +1,9 @@
 package br.elissonsouza.controleordens;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,14 +15,48 @@ public class Database {
 
     private Database() {
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:controle-ordens/src/main/resources/dados.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-            String sql = FileUtils.loadTextFile("controle-ordens/src/main/resources/inicializacao.sql");
-            statement.executeUpdate(sql);
+            connection = DriverManager.getConnection("jdbc:sqlite::resource:dados.db");
+
+            String sql = carregarArquivoDeRecurso("inicializacao.sql");
+            
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sql);
+            }            
 
         } catch (Exception e) {
             System.err.println("Houve um erro ao criar o banco de dados: " + e.getMessage());
+        }
+    }
+
+    private static String carregarArquivoDeRecurso(String nomeArquivo) {
+        // Obter o ClassLoader da classe atual
+        ClassLoader classLoader = Database.class.getClassLoader();
+    
+        // Carregar o arquivo de recurso como um InputStream
+        try (InputStream inputStream = classLoader.getResourceAsStream(nomeArquivo)) {
+            // Se o InputStream não for nulo (ou seja, se o recurso for encontrado)
+            if (inputStream != null) {
+                // Cria um InputStreamReader para ler o conteúdo do InputStream
+                InputStreamReader streamReader = new InputStreamReader(inputStream);
+                // Usa um BufferedReader para ler o conteúdo linha por linha
+                BufferedReader reader = new BufferedReader(streamReader);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                // Lê cada linha do arquivo e adiciona ao StringBuilder
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\n");
+                }
+                return stringBuilder.toString();
+
+            } else {
+                System.err.println("Arquivo de recurso não encontrado: " + nomeArquivo);
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
